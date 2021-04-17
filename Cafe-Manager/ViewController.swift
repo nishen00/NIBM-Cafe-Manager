52234eedc8f50c0fb8acdb0d5a308b88fc41284f
bhanuka nishendra <45203195+nishen00@users.noreply.github.com>
2021-04-17 07:42:19 +0530

    Initial Commit

---
+++ Cafe-Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cafe-Manager
+//
+//  Created by Bhanuka Nishen on 2021-04-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
